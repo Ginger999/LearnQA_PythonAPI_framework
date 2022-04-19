de3eaa34afa32cf7c3082b6ee65348a223bc66a5
Ginger999 <sansara_bk@.ru>
2022-04-19 07:27:55 +0300

    added single quotes in message

--- lib/base_case.py
+++ lib/base_case.py
@@ -16,9 +16,9 @@ class BaseCase:
         try:
             response_as_dict = response.json()
         except json.decoder.JSONDecodeError:
-            assert False, f"Response is not in JSON format. Response text is {response.text}"
+            assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
-        assert name in response_as_dict, f"Response JSON does not have key {name}"
+        assert name in response_as_dict, f"Response JSON does not have key '{name}'"
 
         return response_as_dict[name]
 
