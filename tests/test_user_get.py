68a46f40acfd7906d65dd6c54e85f69421c9e216
Ginger999 <sansara_bk@.ru>
2022-04-21 09:23:42 +0300

    added test_get_user_details_auth_as_another_user

--- tests/test_user_get.py
+++ tests/test_user_get.py
@@ -4,7 +4,8 @@ from lib.assertions import Assertions  # import lib.assertions as Assertions
 from lib.base_case import BaseCase  # import lib.base_case as BaseCase
 from lib.my_requests import MyRequests  # import lib.my_requests as MyRequests
 
-@allure.epic("Info getting cases")
+
+@allure.epic("User info getting cases")
 class TestUserGet(BaseCase):
     def test_get_user_details_not_auth(self):
         response = MyRequests.get("/user/2")
@@ -34,3 +35,35 @@ class TestUserGet(BaseCase):
 
         expected_fields = ["username", "email", "firstName", "lastName"]
         Assertions.assert_json_has_keys(response2, expected_fields)
+
+    @allure.description("This test logins by User1 and tries to get User2 info")
+    def test_get_user_details_auth_as_another_user(self):
+        # Register User1
+        register_data = self.prepare_registration_data()
+        response_1 = MyRequests.post("/user", data=register_data)
+
+        Assertions.assert_status_code(response_1, 200)
+        Assertions.assert_json_has_key(response_1, "id")
+
+        user_id_1 = self.get_json_value(response_1, "id")
+
+        # Login by existing User2
+        response_2_1 = MyRequests.post("/user/login", data={'email': 'vinkotov@example.com', 'password': '1234'})
+
+        Assertions.assert_status_code(response_2_1, 200)
+
+        token_2 = self.get_header(response_2_1, "x-csrf-token")
+        user_id_from_auth_method_2 = self.get_json_value(response_2_1, "user_id")
+
+        # Get User1 data fields
+        response_2_2 = MyRequests.get(
+            f"/user/{user_id_from_auth_method_2}",
+            headers={"x-csrf-token": token_2},
+            cookies={"auth_sid": user_id_1}
+        )
+
+        expected_fields = ["username"]
+        Assertions.assert_json_has_keys(response_2_2, expected_fields)
+
+        unexpected_fields = ["email", "firstName", "lastName"]
+        Assertions.assert_json_has_not_keys(response_2_2, unexpected_fields)
