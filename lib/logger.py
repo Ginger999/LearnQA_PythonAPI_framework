f6a868f6a73b18b4c673b7324d88ecfcd8fd733f
Ginger999 <sansara_bk@.ru>
2022-04-21 09:28:39 +0300

    fixed: file_name with \\ instead of \

--- lib/logger.py
+++ lib/logger.py
@@ -6,7 +6,7 @@ from environment import ENV_OBJECT
 
 
 class Logger:
-    file_name = f"\logs\log_" + str(datetime.now().strftime("%y-%m-%d_%H-%M-%S")) + ".log"
+    file_name = f"\\logs\\log_" + str(datetime.now().strftime("%y-%m-%d_%H-%M-%S")) + ".log"
     # От каталога текущего модуля вврх по дереву каталогов
     full_file_name = os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0], f"..{file_name}"))
 
