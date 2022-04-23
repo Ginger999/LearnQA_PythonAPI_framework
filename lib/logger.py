68ea234fb6b2f417bbd0ba561a382de328892c48
Ginger999 <sansara_bk@.ru>
2022-04-23 22:03:53 +0300

    fixed: add_response writes now

--- lib/logger.py
+++ lib/logger.py
@@ -41,6 +41,6 @@ class Logger:
         data_to_add += f"Response text: {response.text}\n"
         data_to_add += f"Response headers: {headers_as_dict}\n"
         data_to_add += f"Response cookies: {cookies_as_dict}\n"
-        data_to_add = f"\n-----\n"
+        data_to_add += f"\n-----\n"
 
         cls._write_log_to_file(data_to_add)
