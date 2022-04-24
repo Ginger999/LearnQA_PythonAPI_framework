d0d8ca9321ebd8f61d0447a533eac09d19edb1ed
Ginger999 <sansara_bk@.ru>
2022-04-24 23:48:31 +0300

    fixed: assert_auth_token_not_supplied added: assert_too_short_param_value changed: some prints

--- lib/assertions.py
+++ lib/assertions.py
@@ -11,7 +11,7 @@ class Assertions:
             assert False, f"Response is not in JSON format. Response text is '{response.text}'"
 
         assert name in response_as_dict, f"Response JSON does not have key '{name}'"
-        assert response_as_dict[name] == expected_value, f"{error_message} - Actual: '{response_as_dict[name]}' Expected: '{expected_value}'"
+        assert response_as_dict[name] == expected_value, f"Expected: '{expected_value}' | Actual: '{response_as_dict[name]}' {error_message} "
 
     @staticmethod
     def assert_json_has_key(response: Response, name):
@@ -57,26 +57,39 @@ class Assertions:
             f"Unexpected status code! Expected: {expected_status_code} Actual: {response.status_code}"
 
     @staticmethod
+    def assert_unexpected_status_code(response: Response, unexpected_status_code):
+        assert response.status_code != unexpected_status_code, \
+            f"Status code should not be equal {unexpected_status_code}"
+
+    @staticmethod
     def assert_required_params(response: Response, param):
-        assert response.content.decode("utf-8") == f"The following required params are missed: {param}",\
+        assert response.content.decode("utf-8") == f"The following required params are missed: {param}", \
             f"The following required params are missed: '{param}"
 
     @staticmethod
-    def assert_too_short_param_value(response: Response, param, value):
-        assert response.content.decode("utf-8") == f"The value of '{param}' field is too short", \
-             f"The value of '{param}' field is too short '{value}'"
-
-    @staticmethod
     def assert_too_long_param_value(response: Response, param, value):
-        assert response.content.decode("utf-8") == f"The value of '{param}' field is too long",\
+        assert response.content.decode("utf-8") == f"The value of '{param}' field is too long", \
             f"The value of '{param}' field is too long {value.__len__()}"
 
     @staticmethod
     def assert_auth_token_not_supplied(response: Response, param, value):
-        assert response.content.decode("utf-8") == f"Auth token not supplied",\
+        assert response.content.decode("utf-8") != f"Auth token not supplied1", \
             f"The attempt to changed '{param}' with '{value}' for unlogged user"
 
     @staticmethod
     def assert_invalid_email_format(response: Response, email):
         assert response.content.decode("utf-8") == f"Invalid email format", \
             f"Invalid email format '{email}'"
+
+    @staticmethod
+    def assert_too_short_param_value(response: Response, param, value):
+        err_msg_1 = f"The value of '{param}' field is too short"
+        err_msg_2 = f"Too short value for field {param}"
+
+        content = response.content.decode("utf-8")
+
+        assert (content.find(err_msg_1) > -1 or content.find(err_msg_2) > -1), f"{err_msg_1} '{value}'"
+
+
+
+
