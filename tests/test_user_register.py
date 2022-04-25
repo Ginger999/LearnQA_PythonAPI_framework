8c7aa48dd0203e5e873ef147f17fd4ddf59bf058
Ginger999 <sansara_bk@.ru>
2022-04-25 21:08:41 +0300

    modified: allure descriptions

--- tests/test_user_register.py
+++ tests/test_user_register.py
@@ -16,7 +16,7 @@ class TestUserRegister(BaseCase):
         ("email")
     ]
 
-    @allure.description("")
+    @allure.description("This test creates user successfully")
     def test_create_user_successfully(self):
         """
         :return: JSON {'data': data, 'response': response}
@@ -28,7 +28,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_json_has_key(response, "id")
         return {'data': data, 'response': response}
 
-    @allure.description("")
+    @allure.description("This test tries to create a user that already exists")
     def test_create_user_with_existing_email(self):
         existing_email = self.existing_user_data['email']
         data = self.prepare_registration_data(existing_email)
@@ -39,7 +39,7 @@ class TestUserRegister(BaseCase):
         assert response.content.decode("utf-8") == f"Users with email '{existing_email}' already exists", \
             f"Unexpected response content '{response.content}'"
 
-    @allure.description("")
+    @allure.description("This test tries to create a user using invalid email format")
     def test_invalid_email_format(self):
         data = self.prepare_registration_data()
         data['email'] = self.prepare_invalid_format_email(data['email'])
@@ -50,10 +50,9 @@ class TestUserRegister(BaseCase):
         assert response.content.decode(
             "utf-8") == f"Invalid email format", f"Invalid email format {data['email']}"
 
-    @allure.description("")
+    @allure.description("This test checks that the needed param of user is present")
     @pytest.mark.parametrize('param', required_params)
     def test_required_params(self, param):
-        data = self.prepare_registration_data()
         del data[param]
 
         response = MyRequests.post("/user/", data=data)
@@ -61,7 +60,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         Assertions.assert_required_params(response, param)
 
-    @allure.description("")
+    @allure.description("This test tries to create a user using too short username")
     def test_short_username(self):
         data = self.prepare_registration_data()
         param = 'username'
@@ -72,7 +71,7 @@ class TestUserRegister(BaseCase):
         Assertions.assert_status_code(response, 400)
         Assertions.assert_too_short_param_value(response, param, data[param])
 
-    @allure.description("")
+    @allure.description("This test tries to create a user using too long username")
     def test_too_long_username(self):
         data = self.prepare_registration_data()
         param = 'username'
